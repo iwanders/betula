54aad43737b0ee349e3264b12c8ecdf2b1374c6b
Ivor Wanders <ivor@iwanders.net>
2024-04-14 12:34:57 -0400

    Make delay time configuration nice.

--- README.md
+++ README.md
@@ -2,8 +2,6 @@
 
 Work in progress behaviour tree library.
 
-## Todo
-- [ ] Check against [rust api guidelines](https://rust-lang.github.io/api-guidelines/about.html).
 
 ## License
 License is [`BSD-3-Clause`](./LICENSE).
--- betula_egui/src/nodes/common.rs
+++ betula_egui/src/nodes/common.rs
@@ -5,12 +5,58 @@ use betula_common::nodes;
 
 impl UiNode for nodes::DelayNode {
     fn ui_config(&mut self, ui: &mut Ui, _scale: f32) -> UiConfigResponse {
-        let r = ui.add(egui::Slider::new(&mut self.config.interval, 0.0..=100.0).text("My value"));
-        if r.changed() {
-            // println!("Changed! now: {}", self.config.interval);
-            return UiConfigResponse::Changed;
-        }
-        UiConfigResponse::UnChanged
+        let mut ui_response = UiConfigResponse::UnChanged;
+        ui.horizontal(|ui| {
+            ui.label("Delay: ");
+
+            let speed = if self.config.interval < 1.0 {
+                0.001
+            } else if self.config.interval < 10.0 {
+                0.01
+            } else {
+                0.1
+            };
+
+            let r = ui.add(
+                egui::DragValue::new(&mut self.config.interval)
+                    .clamp_range(0.0f64..=(24.0 * 60.0 * 60.0))
+                    .speed(speed)
+                    .custom_formatter(|v, _| {
+                        if v < 10.0 {
+                            format!("{:.0} ms", v * 1000.0)
+                        } else if v < 60.0 {
+                            format!("{:.3} s", v)
+                        } else {
+                            format!("{:.0} s", v)
+                        }
+                    })
+                    .custom_parser(|s| {
+                        let parts: Vec<&str> = s.split(' ').collect();
+                        let value = parts[0].parse::<f64>().ok()?;
+                        if let Some(scale) = parts.get(1) {
+                            if *scale == "ms" {
+                                Some(value * 0.001)
+                            } else if *scale == "s" {
+                                Some(value)
+                            } else if *scale == "m" {
+                                Some(value * 60.0)
+                            } else {
+                                None
+                            }
+                        } else {
+                            Some(value)
+                        }
+                    })
+                    .update_while_editing(false),
+            );
+
+            if r.changed() {
+                // println!("Changed! now: {}", self.config.interval);
+                ui_response = UiConfigResponse::Changed;
+            }
+        });
+
+        ui_response
     }
     fn ui_child_range(&self) -> std::ops::Range<usize> {
         0..1
