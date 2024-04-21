68d180219430849cd42d6903b76db0878a66c67b
Ivor Wanders <ivor@iwanders.net>
2024-04-21 12:02:33 -0400

    Use fork, expose retrieval functions.

--- betula_global_hotkey/examples/test_global_hotkey.rs
+++ betula_global_hotkey/examples/test_global_hotkey.rs
@@ -1,11 +1,25 @@
 use betula_global_hotkey::GlobalHotkeyInterface;
+use global_hotkey::hotkey::{Code, HotKey, Modifiers};
 
 pub fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let runner = GlobalHotkeyInterface::new()?;
+    let mut runner = GlobalHotkeyInterface::new()?;
 
-    let mut sleep_interval = std::time::Duration::from_millis(10);
+    let hotkey = HotKey::new(Some(Modifiers::SHIFT), Code::KeyD);
+    runner.register(hotkey)?;
+    let hotkey_without_mods = HotKey::new(None, Code::KeyQ);
+    runner.register(hotkey_without_mods)?;
+
+    let mut sleep_interval = std::time::Duration::from_millis(100);
     loop {
         std::thread::sleep(sleep_interval);
+        for k in [hotkey, hotkey_without_mods] {
+            println!(
+                "{k:?}: is_pressed: {}, is_toggled: {}",
+                runner.is_pressed(k)?,
+                runner.is_toggled(k)?
+            );
+        }
+        println!();
     }
     Ok(())
 }
