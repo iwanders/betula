998b5913cb189811ff296f6cbc61b38d17c24414
Ivor Wanders <ivor@iwanders.net>
2024-05-26 08:58:06 -0400

    Put the min radius into the spiral.

--- betula_enigo/src/util.rs
+++ betula_enigo/src/util.rs
@@ -1,13 +1,27 @@
+/// An object to create coordinates on an Archimedean spiral
 pub struct Spiral {
+    /// The x coordinate of the center of the spiral.
     pub x: f64,
+    /// The y coordinate of the center of the spiral.
     pub y: f64,
+    /// The speed at which the spiral is traversed (in arc speed).
     pub speed: f64,
+    /// The maximum radius of the spiral, at this point the parameter is reset.
     pub max_radius: f64,
+    /// The a component in the (a + b * t) spiral radius.
     pub a: f64,
+    /// The b component in the (a + b * t) spiral radius.
     pub b: f64,
+    /// The current parameter, advanced by dt * speed each advance.
     pub parameter: f64,
+    /// The minimum radius, below this the path parameter is advanced.
+    pub min_radius: f64,
+    /// The timestep to achieve the minimum radius.
+    pub min_radius_dt: f64,
 }
+
 impl Spiral {
+    /// Create a new spiral with a max radius.
     pub fn new(c: (f64, f64), a: f64, b: f64, speed: f64, max_radius: f64) -> Self {
         let (x, y) = c;
         Self {
@@ -18,9 +32,12 @@ impl Spiral {
             b,
             max_radius,
             parameter: 0.0,
+            min_radius: 0.0,
+            min_radius_dt: 0.01, // prevent footgun for infinite loops.
         }
     }
 
+    /// Advance the spiral to a certain radius.
     pub fn advance_to_radius(&mut self, radius: f64, dt: f64) -> (f64, f64) {
         let (x, y) = self.advance(0.0);
         let (x, y) = (x - self.x, y - self.y);
@@ -35,17 +52,19 @@ impl Spiral {
         self.advance(0.0)
     }
 
+    /// Advance the spiral with a dt and return the new position.
     pub fn advance(&mut self, dt: f64) -> (f64, f64) {
         // use std::f64::consts::PI;
         // https://gamedev.stackexchange.com/a/16756
         //   spiral fun: (cos(t) * f(t), sin(t) * f(t))
         //   w(t) = V / (2 * pi * f (t))
-        // Archimedal:
+        // Archimedean:
         //   fun (cos(t) * t, sin(t) * t)
         //   w(t) = V / (2 * pi * t)
         // Parametrized:
         //    f(t) = r = a + b*t
         //    w(t) = v / (2 * pi * (a + b * t));
+        // The above is ignored for now, I didn't really need it just yet.
 
         self.parameter = self.parameter + dt * self.speed;
         let t = self.parameter;
@@ -57,13 +76,10 @@ impl Spiral {
 
         if r >= self.max_radius {
             self.parameter = 0.0;
+            self.advance_to_radius(self.min_radius, self.min_radius_dt);
         }
 
         (x + self.x, y + self.y)
-    }
-
-    pub fn parameter(&self) -> f64 {
-        self.parameter
     }
 }
 
